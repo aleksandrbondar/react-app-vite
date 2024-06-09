0c00eda3f20790e0121be95c60bef756e55046fb
aleksandrbondar <aleksandr_bondar@icloud.com>
2024-06-09 11:45:11 +0300

    v1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='title'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
